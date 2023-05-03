62b84602f4c42b8f3dda40f917ada48833503b59
adaramendiz <andres-aramendiz@hotmail.com>
2023-05-02 23:45:11 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { getRandomNumber } from "./utils/getRandom";
+import phrases from "./assets/phrases.json";
+import Phrase from "./components/Phrase/Phrase";
+import ChangePhraseButton from "./components/Phrase/ChangePhraseButton";
+
+import "./App.css";
+import space1 from "./assets/space_1.jpg";
+import space2 from "./assets/space_2.jpg";
+
+const backgrounds = [space1, space2, "/space_3.jpg", "/space_4.jpg"];
+
+function App() {
+  const getRandomPhrase = () => phrases[getRandomNumber(phrases.length - 1)];
+  const getRandomBackground = () => backgrounds[getRandomNumber(backgrounds.length - 1)];
+
+  const [phraseObject, setPhraseObject] = useState(getRandomPhrase());
+  const [background, setBackground] = useState(getRandomBackground());
+  const [newPhrase, setNewPhrase] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+
+  const changePhrase = () => {
+    let newPhraseObj = getRandomPhrase();
+    let newBackground = getRandomBackground();
+
+    while (newPhraseObj.phrase === phraseObject.phrase) {
+      newPhraseObj = getRandomPhrase();
+    }
+
+    while (newBackground === background) {
+      newBackground = getRandomBackground();
+    }
+
+    setPhraseObject(newPhraseObj);
+    setBackground(newBackground);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPhraseObj = { phrase: newPhrase, author: newAuthor };
+    setPhraseObject(newPhraseObj);
+    setNewPhrase("");
+    setNewAuthor("");
+  };
+
+  return (
+    <div className="container_app" style={{ backgroundImage: `url("${background}")` }}>
+      <h1 className="titulo">INFOGALAXY</h1>
+      <Phrase phrase={phraseObject.phrase} />
+      <p className="author">
+        Autor: <mark>{phraseObject.author}</mark>
+      </p>
+      <ChangePhraseButton changePhrase={changePhrase} />
+      <div className="form-container">
+      <form onSubmit={handleSubmit}>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+export default App;
